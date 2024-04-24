5bf8a76cc785eb6c58e09ec4559bf746f201f8ae
Renan Pereira Loureiro <renanloureiro.dev@gmail.com>
2024-04-24 11:45:23 -0300

    feat: add create account route

---
+++ src/controllers/create-account.controller.ts
@@ -0,0 +1,53 @@
+import {
+  ConflictException,
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+} from '@nestjs/common'
+import { PrismaService } from 'src/prisma/prisma.service'
+
+import { hash } from 'bcryptjs'
+import { z } from 'zod'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
+
+const createAccountBodySchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string(),
+})
+
+type CreateAccountDTO = z.infer<typeof createAccountBodySchema>
+
+const PASSWORD_SALT_HASH = 8
+
+@Controller('/accounts')
+export class CreateAccountController {
+  constructor(private readonly prisma: PrismaService) {}
+
+  @Post()
+  @UsePipes(new ZodValidationPipe(createAccountBodySchema))
+  async handle(@Body() body: CreateAccountDTO) {
+    const { name, email, password } = body
+
+    const userWithSameEmail = await this.prisma.user.findUnique({
+      where: { email },
+    })
+
+    if (userWithSameEmail) {
+      throw new ConflictException(
+        'User with same e-mail address already exists',
+      )
+    }
+
+    const passwordHashed = await hash(password, PASSWORD_SALT_HASH)
+
+    await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password: passwordHashed,
+      },
+    })
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule)
   await app.listen(3333, () =>
-    console.log(`Sever running: http://localhost:3333`)
-  );
+    console.log(`Sever running: http://localhost:3333`),
+  )
 }
-bootstrap();
+bootstrap()
